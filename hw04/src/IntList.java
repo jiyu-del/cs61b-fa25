6fe50791ab1b4231463beb10114878e83579b344
jiyu <jiyu50095@gmail.com>
2025-12-23 01:33:58 +0800

    homeword4

--- hw04/src/IntList.java
+++ hw04/src/IntList.java
@@ -43,7 +43,10 @@ public class IntList {
      */
     public static IntList incrRecursiveNondestructive(IntList L, int x) {
         // TODO: Fill in this code
-        return null;
+        if (L == null) return null;
+        IntList M = new IntList(L.first + x, null);
+        M.rest = incrRecursiveNondestructive(L.rest, x);
+        return M;
     }
 
     /**
@@ -53,7 +56,10 @@ public class IntList {
      */
     public static IntList incrRecursiveDestructive(IntList L, int x) {
         // TODO: Fill in this code
-        return null;
+        if (L == null) return null;
+        L.first += x;
+        incrRecursiveDestructive(L.rest, x);
+        return L;
     }
 
     /**
